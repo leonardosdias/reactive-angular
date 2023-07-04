41616c0a524adaab8b91e88b486e592f144c8b5b
Leonardo Dias <leonnardosouza96@gmail.com>
2023-07-04 12:29:15 -0300

    Estrutura inicial refatoração

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  loadlAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(resp => resp['payload']),
+        shareReplay()
+      );
+  }
+}
